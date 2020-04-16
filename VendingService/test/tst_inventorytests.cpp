b51be14b87e6296efdb0471766064a2f130ee28c
eejohnson1 <erik.johnson0110@gmail.com>
2020-04-16 14:23:34 -0500

    added more tests

--- VendingService/test/tst_inventorytests.cpp
+++ VendingService/test/tst_inventorytests.cpp
@@ -18,6 +18,10 @@ private slots:
     void test_case_add_item_return_false_if_one_exists_already();
     void test_case_item_added();
     void test_case_item_not_added();
+    void test_case_remove_item_true();
+    void test_case_remove_item_false();
+    void test_case_get_item_returns_item();
+    void test_case_get_item_returns_nullptr();
 
 };
 
@@ -123,6 +127,61 @@ void inventorytests::test_case_item_not_added()
     // verify first item is still in inventory, not second
     QCOMPARE(&(*testExistingItem), result);
 
+}
+
+// Test verifies that removeItem returns true if an item was found and removed
+void inventorytests::test_case_remove_item_true(){
+    unique_ptr<Inventory> inv = make_unique<Inventory>();
+    string testItemCode = "T1";
+
+    // Add an item to inventory in slot "T1"
+    unique_ptr<Item> testExistingItem = make_unique<Item>();
+    testExistingItem->setName("Mountain Dew");
+    testExistingItem->setQuantity(12);
+    testExistingItem->setSize(20.0);
+    testExistingItem->setSizeUnits("Fluid Ounce");
+    inv->addItem(testItemCode, *testExistingItem);
+
+    // remove the item
+    bool result = inv->removeItem(testItemCode);
+    QCOMPARE(result, true);
+
+}
+
+// Test verifies that removeItem returns false if an item was not found and removed.
+void inventorytests::test_case_remove_item_false(){
+    unique_ptr<Inventory> inv = make_unique<Inventory>();
+    string testItemCode = "T1";
+
+    bool result = inv->removeItem(testItemCode);
+    QCOMPARE(result, false);
+}
+
+// test verifies that getItem returns a pointer to the original item
+void inventorytests::test_case_get_item_returns_item(){
+    unique_ptr<Inventory> inv = make_unique<Inventory>();
+    string testItemCode = "T1";
+
+    // Add an item to inventory in slot "T1"
+    unique_ptr<Item> testExistingItem = make_unique<Item>();
+    testExistingItem->setName("Mountain Dew");
+    testExistingItem->setQuantity(12);
+    testExistingItem->setSize(20.0);
+    testExistingItem->setSizeUnits("Fluid Ounce");
+    inv->addItem(testItemCode, *testExistingItem);
+
+    // remove the item
+    Item *result = inv->getItemByVendCode(testItemCode);
+    QCOMPARE(result, &(*testExistingItem));
+}
+
+// test verifies that getItem returns a nullptr when no item found.
+void inventorytests::test_case_get_item_returns_nullptr(){
+    unique_ptr<Inventory> inv = make_unique<Inventory>();
+    string testItemCode = "T1";
+
+    Item *result = inv->getItemByVendCode(testItemCode);
+    QCOMPARE(result, nullptr);
 }
 
 QTEST_APPLESS_MAIN(inventorytests)
